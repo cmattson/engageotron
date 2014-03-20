288209e61d695b563d5e42dadfd37f6941c70715
Colin Mattson <colinmattson@gmail.com>
2014-03-20 14:59:18 -0700

    Contact model docs, schema

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
+# Contact records represent both members and nonmembers.
+#
+# These records are generally created programatically as the result of 
+# importing a membership roster or an event attendance report.
 class Contact < ActiveRecord::Base
+  MEMBER_TYPE = {
+  }
 end
