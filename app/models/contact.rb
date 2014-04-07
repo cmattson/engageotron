9af066293477cb3f3b38ba22d18e5e93dcb2f6ce
Colin Mattson <colinmattson@gmail.com>
2014-04-07 12:58:16 -0700

    Scopes for membership age

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,6 +12,11 @@ class Contact < ActiveRecord::Base
   default_scope { order('last_name ASC, first_name ASC') }
   scope :newest, -> { unscoped.where(member: true).order('member_since DESC').limit(5) }
   
+  # Scopes for join date reporting
+  scope :join_dates, -> { unscoped.where(member: true).order(:member_since).pluck(:member_since) }
+  scope :last_joined, -> { unscoped.where(member: true).order('member_since DESC').limit(1) }
+  scope :first_joined, -> { unscoped.where(member:true).order('member_since ASC').limit(1) }
+  
   # Returns the contact's full name (first and last names).
   def full_name
     [first_name, last_name].join(' ')
@@ -20,5 +25,17 @@ class Contact < ActiveRecord::Base
   # Returns the contact's name in directory format (last name first).
   def directory_name
     [last_name, first_name].join(', ')
+  end
+  
+  # Returns median join date of members
+  def self.median_age
+    times = join_dates.map{ |x| x.to_time.to_i }
+    # If even array, mean the two middle values.
+    if times.size.even?
+      Time.at((times[times.size / 2] + times[times.size / 2 - 1]) / 2).to_date
+    # If odd array, use the true median.
+    else
+      Time.at(times[times.size / 2]).to_date
+    end
   end
 end
