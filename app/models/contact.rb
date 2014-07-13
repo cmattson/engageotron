00711a3870544a169c1e9cd30d23235a53a5b5db
Colin Mattson <colinmattson@gmail.com>
2014-07-13 14:48:14 -0700

    HACK: Temporary fix for dashboard age display

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,12 +11,12 @@ class Contact < ActiveRecord::Base
 
   default_scope { order('last_name ASC, first_name ASC') }
   scope :newest, -> { unscoped.where(member: true).order('member_since DESC').limit(5) }
-  
+
   # Scopes for join date reporting
   scope :join_dates, -> { unscoped.where(member: true).order(:member_since).pluck(:member_since) }
   scope :last_joined, -> { unscoped.where(member: true).order('member_since DESC').limit(1) }
   scope :first_joined, -> { unscoped.where(member:true).order('member_since ASC').limit(1) }
-  
+
   # Returns the contact's full name (first and last names).
   def full_name
     [first_name, last_name].join(' ')
@@ -26,26 +26,26 @@ class Contact < ActiveRecord::Base
   def directory_name
     [last_name, first_name].join(', ')
   end
-  
+
   # Returns median join date of members
   def self.median_age
-    times = join_dates.map{ |x| x.to_time.to_i }
+    times = join_dates.map { |x| x.to_time.to_i }
     # If no members, return N/A
     if times.empty?
       'N/A'
     # If even array, mean the two middle values.
     elsif times.size.even?
-      Time.at((times[times.size / 2] + times[times.size / 2 - 1]) / 2).to_date
+      Time.at((times[times.size / 2] + times[times.size / 2 - 1]) / 2).to_date.stftime('%D')
     # If odd array, use the true median.
     else
-      Time.at(times[times.size / 2]).to_date
+      Time.at(times[times.size / 2]).to_date.strftime('%D')
     end
   end
-  
+
   def self.newest_join_date
-    last_joined.empty? ? 'N/A' : last_joined.first.member_since.strftime("%D")
+    last_joined.empty? ? 'N/A' : last_joined.first.member_since.strftime('%D')
   end
-  
+
   def self.oldest_join_date
     first_joined.empty? ? 'N/A' : first_joined.first.member_since.strftime('%D')
   end
